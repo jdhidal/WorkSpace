2fbfaee1220b2b05160280f7774c6215cd9da519
jdhidal <diegohidalgo673@gmail.com>
2025-01-19 12:42:12 -0500

    feat: :sparkles: Rename Code in app.py

--- USER MANAGEMENT/Logout Session/src/app.py
+++ USER MANAGEMENT/Logout Session/src/app.py
@@ -4,32 +4,30 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
-# Cargar variables de entorno desde el archivo .env
+# Uploaded .env
 load_dotenv()
 
 app = Flask(__name__)
 
-# Ruta para Swagger UI
+# Routes Swagger
 SWAGGER_URL = '/api-docs'  
-API_URL = '/swagger.yaml'  # Aquí usamos el archivo swagger.yaml directamente
+API_URL = '/swagger.yaml'  
 
-# Configura Swagger UI para usar el archivo swagger.yaml
+# Configurated Swagger
 swagger_ui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "Logout Service"})
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
-# Ruta para servir el archivo swagger.yaml
+
 @app.route(API_URL)
 def swagger_yaml():
-    # Sirve el archivo swagger.yaml desde el directorio actual
     return send_from_directory('.', 'swagger.yaml')
 
-# Configurar CORS desde la variable de entorno
+# Configuration CORS 
 CORS(app, resources={r"/api/*": {"origins": os.getenv('FRONTEND_URL')}}, supports_credentials=True)
 
-# Ruta para hacer logout
+
 @app.route('/logout', methods=['POST'])
 def logout():
-    # Lógica para el logout
     return jsonify({"message": "Logout successful"})
 
 if __name__ == "__main__":
