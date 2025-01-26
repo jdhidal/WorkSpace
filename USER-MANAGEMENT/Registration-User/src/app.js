19ad2d13621110dca1d05998da0e6e7da0f8aaef
jdhidal <diegohidalgo673@gmail.com>
2025-01-25 21:54:32 -0500

    feat: :sparkles: Add Dockerfile and Uploaded in CloudProvider

--- USER-MANAGEMENT/Registration-User/src/app.js
+++ USER-MANAGEMENT/Registration-User/src/app.js
@@ -10,19 +10,14 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware CORS
 app.use(cors());
 
-// Middleware para manejar JSON
 app.use(express.json());
 
-// Cargar Swagger YAML
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
-// Configurar Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Endpoint para crear usuarios
 app.post('/create', createUser);
 
 const port = process.env.PORT || 3001;
