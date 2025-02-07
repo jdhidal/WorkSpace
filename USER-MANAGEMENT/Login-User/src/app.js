94b256530ac2fdf5bd0917dcf939c2292da9df5b
jdhidal <diegohidalgo673@gmail.com>
2025-02-07 18:31:07 -0500

    test: :rocket: Test pipeline of CI

--- USER-MANAGEMENT/Login-User/src/app.js
+++ USER-MANAGEMENT/Login-User/src/app.js
@@ -33,5 +33,5 @@ app.post('/login', userController.login);
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
-  console.log(`Authentication service running on http://localhost:${port}`);
+  console.log(`Authentication service running http://localhost:${port}`);
 });
