bc9eeeb7ae8a8681ba959622023bba35a146fbe8
jdhidal <diegohidalgo673@gmail.com>
2025-02-08 13:40:39 -0500

    test: :rocket: Test 2 use pipeline

--- AVAILABILITY-MANAGEMENT/Create-Availabilty/main.go
+++ AVAILABILITY-MANAGEMENT/Create-Availabilty/main.go
@@ -15,6 +15,6 @@ func main() {
 	if port == "" {
 		port = "3007"
 	}
-	fmt.Println("🚀 Run Service in port use Instance AWS", port)
+	fmt.Println("🚀 Run Service in port use Instance AWS test 2", port)
 	r.Run(":" + port)
 }
