7d045e359eba062caf9e6f5a67d84b1da1c5a4a0
jdhidal <diegohidalgo673@gmail.com>
2025-02-08 11:14:30 -0500

    test: :rocket: Test new pipeline in API LoginUser

--- USER-MANAGEMENT/Login-User/src/app.js
+++ USER-MANAGEMENT/Login-User/src/app.js
@@ -35,5 +35,5 @@ app.post('/login', userController.login);
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
-  console.log(`Authentication service http://localhost:${port}`);
+  console.log(`Authentication service use Instance AWS http://localhost:${port}`);
 });
