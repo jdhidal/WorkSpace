554dc681c80d82c071324ddad7d31a484153f369
KevinGuana28 <kevinguaa28@gmail.com>
2025-02-11 22:32:07 -0500

    feat: :sparkles: delete comments

--- COMMENT-MANAGEMENT/List-Comment/src/controllers/commentController.js
+++ COMMENT-MANAGEMENT/List-Comment/src/controllers/commentController.js
@@ -1,17 +1,14 @@
-// src/controllers/commentController.js
-// src/controllers/commentController.js
-const Comment = require("../models/commentModel"); // Actualiza la ruta para que coincida con la ubicación correcta
+const Comment = require("../models/commentModel"); 
 
 
-// Función para obtener todos los comentarios
 const getComments = async (req, res) => {
     try {
-        const comments = await Comment.find(); // Obtienes todos los comentarios de la base de datos
-        res.status(200).json(comments); // Respondes con los comentarios
+        const comments = await Comment.find(); 
+        res.status(200).json(comments); 
     } catch (error) {
         console.error("Error getting comments:", error);
-        res.status(500).json({ message: "Error fetching comments" }); // Manejo de errores
+        res.status(500).json({ message: "Error fetching comments" });
     }
 };
 
-module.exports = { getComments }; // Exportas la función
+module.exports = { getComments }
