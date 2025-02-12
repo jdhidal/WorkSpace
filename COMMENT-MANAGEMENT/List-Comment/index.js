b120ec79b6f59d4162b40c332aa031793b130554
KevinGuana28 <kevinguaa28@gmail.com>
2025-02-11 22:20:46 -0500

    feat: :rocket: delete comments

--- COMMENT-MANAGEMENT/List-Comment/index.js
+++ COMMENT-MANAGEMENT/List-Comment/index.js
@@ -3,14 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./db/connection");
-const commentRoutes = require("./src/routes/commentRoutes");  // Importa las rutas
+const commentRoutes = require("./src/routes/commentRoutes"); 
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/list-comments", commentRoutes);  // Aquí definimos que /list-comments usará commentRoutes
+app.use("/list-comments", commentRoutes); 
 
 const PORT = process.env.PORT || 3016;
 
