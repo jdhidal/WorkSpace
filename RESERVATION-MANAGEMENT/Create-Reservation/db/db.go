c2cb67d894bdf044ace7c3cdbac0b2ef6b61dd06
KevinGuana28 <kevinguaa28@gmail.com>
2025-02-12 00:55:40 -0500

    feat: :memo: delete the comments

--- RESERVATION-MANAGEMENT/Create-Reservation/db/db.go
+++ RESERVATION-MANAGEMENT/Create-Reservation/db/db.go
@@ -19,23 +19,19 @@ func InitDB() {
 		log.Fatalf("Error al cargar el archivo .env: %v", err)
 	}
 
-	// Obtener valores de las variables de entorno
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	name := os.Getenv("DB_NAME")
 
-	// Construir el DSN
 	dsn := user + ":" + password + "@tcp(" + host + ":" + port + ")/" + name + "?charset=utf8&parseTime=True&loc=Local"
 
-	// Conectar a la base de datos
 	DB, err = gorm.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("Error al conectar a la base de datos: %v", err)
 	}
 
-	// Migrar la estructura Reservation
 	DB.AutoMigrate(&models.Reservation{})
 }
 
